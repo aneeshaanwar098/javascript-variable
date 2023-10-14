7bcff95889cebd8d0f0782ed0dfd043ac5660406
muneershehla23 <muneershehla23@gmail.com>
2023-10-14 15:24:56 +0500

    es6 variables

---
+++ script.mjs
@@ -0,0 +1,17 @@
+console.log("es6 variables")
+
+
+
+
+let myschoolname ="GGHSS LARAMA PESHAWAR"
+console.log(myschoolname)
+myschoolname ="GGHSS LARAMA PESHAWAR"
+console.log("frontier")
+
+
+
+let DOB =(2004)
+console.log(DOB)
+
+DOB =(2005)
+console.log(DOB)
